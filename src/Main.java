4549a8487933df07f50db88b5ef6492b14f99400
ali <s.ali.nouri1383@gmail.com>
2023-07-03 13:41:30 +0330

    !!! ???

--- src/Main.java
+++ src/Main.java
@@ -33,46 +33,87 @@ public class Main {
             throw new RuntimeException(e);
         }
     }
-    static void addRestaurant(String name, int x, int y, String foodType, )
+    static void addRestaurant(String Name, int x, int y, String foodType, int managerID, Statement st){
+        try {
+            st.executeUpdate("INSERT INTO restaurant(managerID,foodType,x,y,name) VALUES " +
+                    "(" + managerID + ",\"" + foodType + "\"," + x + "," + y + ",\"" + Name + "\")");
+        } catch (SQLException e) {
+            System.err.println("addRestaurant has error");
+            throw new RuntimeException(e);
+        }
+    }
+    static void addFood(String Name, double price, int resID, Statement st){
+        try {
+            st.executeUpdate("INSERT INTO food(name,price,discount,resID,active,ongoingOrders) VALUES(\""
+                    + Name + "\"," + price + "," + 0 + "," + resID + "," + 0 + "," + 0 + ")");
+        } catch (SQLException e) {
+            System.err.println("addFood has error");
+            throw new RuntimeException(e);
+        }
+    }
     //driver code
     public static void main(String args[]) throws ClassNotFoundException, SQLException {
         Scanner input = new Scanner(System.in);
-        ArrayList<Manager> managers=new ArrayList<>();
         Class.forName("com.mysql.cj.jdbc.Driver");
         String url = "jdbc:mysql://localhost:3306/database";
         Connection con = DriverManager.getConnection(url, "root", "Bale2004");
         Statement st = con.createStatement();
-        int phase=0; //login and register
+        int phase = 0; //login and register
         String command = input.nextLine();
-        String user = "", pass= "";
-        if(phase==0) {
-            switch (command) {
-                case "add manager":
-                    user = input.next();
-                    pass = input.next();
-                    //managers.add(new Manager(1,user,pass));
-                    System.out.println("Welcome Manager." + user + "!");
-                    //System.out.println(managers.get(0).getUserName());
-                    st.executeUpdate("INSERT INTO manager(userName,password) VALUES " + "(\"" + user + "\",\"" + pass + "\")");
-                    phase=1; //restaurant list
-                    break;
-                case "add customer":
-                    user = input.next();
-                    pass = input.next();
-                    System.out.println("Welcome Customer." + user + "!");
-                    st.executeUpdate("INSERT INTO customer(userName,password) VALUES " + "(\"" + user + "\",\"" + pass + "\")");
-                case "login manager":
+        String user = "", pass = "";
+        while (true) {
+            if (phase == 0) {
+                System.out.println("login/register");
+                switch (command) {
+                    case "add manager":
+                        user = input.next();
+                        pass = input.next();
+                        System.out.println("Welcome Manager." + user + "!");
+                        st.executeUpdate("INSERT INTO manager(userName,password) VALUES " + "(\"" + user + "\",\"" + pass + "\")");
+                        phase = 1; //add restaurant
+                        break;
+                    case "add customer":
+                        user = input.next();
+                        pass = input.next();
+                        System.out.println("Welcome Customer." + user + "!");
+                        System.out.println("Please enter x, y, balance!");
+                        int x=input.nextInt();
+                        int y=input.nextInt();
+                        double balance=input.nextDouble();
+                        st.executeUpdate("INSERT INTO customer(userName,password,x,y,balance) VALUES(\""
+                                + user + "\",\"" + pass + "\"," + x + "," + y + "," + balance + ")");
+                        phase=4; //customer's page
+                    case "login manager":
 
-                case "login customer":
+                    case "login customer":
+                }
             }
-        }
-        if(phase==1){
-            ResultSet rs=st.executeQuery("SELECT * FROM manager WHERE userName="+"\'"+user+"\'");
-            int id=0;
-            while(rs.next()) {
-                id = rs.getInt("managerID");
+            int id = 0;
+            if (phase == 1) {
+                ResultSet rs = st.executeQuery("SELECT * FROM manager WHERE userName=" + "\'" + user + "\'");
+                while (rs.next()) {
+                    id = rs.getInt("managerID");
+                }
+                System.out.println("Please enter restaurant name, x, y, food type!");
+                String name = input.next();
+                int x = input.nextInt();
+                int y = input.nextInt();
+                String foodType = input.next();
+                addRestaurant(name, x, y, foodType, id, st);
+                phase = 2; //restaurant page
             }
-            showRestaurants(id,st,rs);
+            if (phase == 2) {
+                System.out.println("Please add food name, price!");
+                while (!input.next().equals("end")) {
+                    String name = input.next();
+                    double price = input.nextDouble();
+                    addFood(name, price, id, st);
+                }
+                phase = 5; //manager's page
+            }
+            if(phase==4){
+
+            }
         }
     }
 }
